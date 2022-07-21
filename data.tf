ba0418fb67954e880e50ab36de71aec76f67f398
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2022-07-21 13:30:30 +0530

    Added ec2 files

---
+++ data.tf
@@ -0,0 +1,26 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_vpc" "talent_academy" {
+
+  filter {
+    name   = "tag:Name"
+    values = ["lab-vpc"]
+  }
+}
+
+data "aws_subnet" "Public" {
+
+  filter {
+    name   = "tag:Name"
+    values = ["Public"]
+  }
+}
