4de450b40a8290f9558bfdfbb99c23845033cbe8
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2022-08-29 19:58:03 +0530

    multi-server upgrade

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,38 @@
-resource "aws_instance" "ansible_server" {
+# resource "aws_instance" "ansible_server" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = "t3.medium"
+#   vpc_security_group_ids = [aws_security_group.ansible_server.id]
+#   key_name = "talent-academy-lab"
+#   subnet_id = data.aws_subnet.Public.id
+
+#   tags = {
+#     Name = "Ansible Server"
+#   }
+# }
+
+# resource "aws_eip" "ansible_server_ip" {
+#   instance = aws_instance.ansible_server.id
+#   vpc      = true
+# }
+
+resource "aws_instance" "ansible_servers" {
+  count = 1
+
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.medium"
+
   vpc_security_group_ids = [aws_security_group.ansible_server.id]
   key_name = "talent-academy-lab"
   subnet_id = data.aws_subnet.Public.id
 
   tags = {
-    Name = "Ansible Server"
+    Name = "Ansible Server ${count.index + 1}"
   }
 }
 
 resource "aws_eip" "ansible_server_ip" {
-  instance = aws_instance.ansible_server.id
+  count = 1
+
+  instance = aws_instance.ansible_servers[count.index].id
   vpc      = true
 }
